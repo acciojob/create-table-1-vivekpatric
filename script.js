c5648a1f8592c89fb0fec44b365f79b4a62c3cd8
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-07-03 12:01:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function insert_Row() {
 	let tr = document.createElement("tr")
 	let td1 = document.createElement("td")
 	let td2 = document.createElement("td")
+	td1.innerText = "";
+	td2.innerText = "";
   td1.innerText = "New Cell1"
 	td2.innerText = "New Cell2"
 	tr.appendChild(td1)
